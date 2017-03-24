4e15fe2cf1fad19a5b811bca94c12bece85bf2a1
Shruti Priya <shrutipriya1808@gmail.com>
2017-03-24 12:23:06 -0700

    add a link to the questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,23 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-
- // Let the route handler add the new question to the Ember Data store and then save it.
+  
   actions: {
     saveQuestion3(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    },
-
-    update(question, params) {
-      Object.keys(params).forEach(function(key){
-        if(params[key] !==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');//move to question route
-    },
+    }
   }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,17 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+
+ // Let the route handler add the new question to the Ember Data store and then save it.
+  actions: {
+    update(question, params) {
+      Object.keys(params).forEach(function(key){
+        if(params[key] !==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('question');//move to question route
+    },
+  }
 });
