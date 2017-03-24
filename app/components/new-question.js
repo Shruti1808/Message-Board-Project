5be64f4e67ba257f17e148d4fce9b20b12823220
Shruti Priya <shrutipriya1808@gmail.com>
2017-03-24 10:00:39 -0700

    add form to add new question by the user and action pertianing to it

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    questionFormHide() {
+      this.set('addNewQuestion', false);
+    },
+  }
 });
