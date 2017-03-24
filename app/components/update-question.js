e8e1be4bdc7b5a37e8fca45390e35e77197d3d5a
Shruti Priya <shrutipriya1808@gmail.com>
2017-03-24 12:49:00 -0700

    trying to fix undefined field value error when trying to edit the question

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -12,38 +12,14 @@ export default Ember.Component.extend({
 
     update(question) {
       var params = {
-        content: this.get('content'),
-        author: this.get('author'),
-        date: this.get('date'),
-        additionalNotes: this.get('additionalNotes'),
-        tag: this.get('tag')
+        content: this.get('content') ? this.get('content') : "",
+        author: this.get('author') ? this.get('author') : "",
+        date: this.get('date') ? this.get('date') : "",
+        additionalNotes: this.get('additionalNotes') ? this.get('additionalNotes') : "",
+        tag: this.get('tag') ? this.get('tag') : ""
       };
       this.set('updateQuestionForm', false);
       this.sendAction('update', question, params);
     }
   }
 });
-
-
-
-
-
-// actions: {
-//     showQuestionForm() {
-//       this.set('updateQuestionForm', true);
-//     },
-//     questionFormHide() {
-//       this.set('updateQuestionForm', false);
-//     },
-//     update(question) {
-//       var params = {
-//         content: this.get('content'),
-//         author: this.get('author'),
-//         date: new Date().toLocaleDateString(),
-//         additionalNotes: this.get('additionalNotes'),
-//         tag: this.get('tag'),
-//       };
-//       this.set('updateQuestionForm', false);
-//       this.sendAction('update', question, params);
-//     }
-//   }
