785e1eda62aa3e27ae8222e8cc2309eeed4644c9
Shruti Priya <shrutipriya1808@gmail.com>
2017-03-24 16:56:41 -0700

    issue with update button to be fixed and need some styling Functionality attained

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   updateQuestionForm: false,
   actions: {
-    showQuestionForm() {
+    updateQuestionForm() {
       this.set('updateQuestionForm', true);
     },
     questionFormHide() {
       this.set('updateQuestionForm', false);
     },
-  
+
     update(question) {
       var params = {
         content: this.get('content') ? this.get('content') : "",
